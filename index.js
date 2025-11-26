559b73acf63fae97e9f0498c338c7c9ca82d6836
Vladisalv Kovalevich <kov.vladislav@tut.by>
2025-11-26 23:08:52 +0100

    ci cd test commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function initDb() {
 
 // Root endpoint to verify the app is running
 app.get('/', async (req, res) => {
-  res.send('Books app is running');
+  res.send('App is running. CI/CD test message');
 });
 
 // GET endpoint to return all books as JSON
