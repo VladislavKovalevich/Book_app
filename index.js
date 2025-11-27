f3785693e45e6bcf1448f29b6d6667d7a25a5e95
Vladisalv Kovalevich <kov.vladislav@tut.by>
2025-11-27 01:18:45 +0100

    test changes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function initDb() {
 
 // Root endpoint to verify the app is running
 app.get('/', async (req, res) => {
-  res.send('App is running. CI/CD test message. qwerty;');
+  res.send('App is running. CI/CD pipline changes. Lorem ipsum test changes');
 });
 
 // GET endpoint to return all books as JSON
