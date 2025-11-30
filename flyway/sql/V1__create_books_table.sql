1acf482f49ab73f430eb3c44126df3ed1ca357b8
Vladisalv Kovalevich <kov.vladislav@tut.by>
2025-11-30 22:32:07 +0100

    add flyway migration tools

---
+++ flyway/sql/V1__create_books_table.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    publish_year INT NOT NULL,
+    description TEXT
+);
---
+++ flyway/sql/V2__insert_books_initial_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO books (title, author, publish_year, description)
+VALUES
+    ('The Hobbit', 'J.R.R. Tolkien', 1937, 'Fantasy'),
+    ('1984', 'George Orwell', 1949, 'Dystopian'),
+    ('To Kill a Mockingbird', 'Harper Lee', 1960, 'Classic Literature'),
+    ('The Pragmatic Programmer', 'Andrew Hunt & David Thomas', 1999, 'Software Engineering');
